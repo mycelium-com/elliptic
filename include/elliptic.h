e76cb8265458921b37a15ac36f6e294415a2d92b
CryptoManiac <balthazar@yandex.ru>
2020-08-15 02:14:18 +0300

    HD keys import and export

--- include/elliptic.h
+++ include/elliptic.h
@@ -16,6 +16,16 @@ typedef struct EllipticContext {
     uint8_t PublicKey[33];
 } EllipticContext;
 
+const unsigned int BIP32_EXTKEY_SIZE = 74;
+
+typedef struct EllipticHDContext {
+    unsigned char nDepth;
+    unsigned char vchFingerprint[4];
+    unsigned int nChild;
+    unsigned char chaincode[32];
+    EllipticContext context;
+} EllipticHDContext;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -23,6 +33,12 @@ extern "C" {
 int elliptic_init(EllipticContext *ctx, int type, const uint8_t *key, const uint8_t *public_key);
 int elliptic_sign(EllipticContext *ctx, const uint8_t *digest, size_t digest_size, uint8_t *signature);
 int elliptic_verify(EllipticContext *ctx, const uint8_t *digest, size_t digest_size, uint8_t *signature, size_t signature_size);
+
+int elliptic_hd_import_pub(EllipticHDContext *ctx, int type, const uint8_t binary[BIP32_EXTKEY_SIZE]);
+int elliptic_hd_import_priv(EllipticHDContext *ctx, int type, const uint8_t binary[BIP32_EXTKEY_SIZE]);
+
+int elliptic_hd_export_pub(EllipticHDContext *ctx, uint8_t binary[BIP32_EXTKEY_SIZE]);
+int elliptic_hd_export_priv(EllipticHDContext *ctx, uint8_t binary[BIP32_EXTKEY_SIZE]);
 
 #ifdef __cplusplus
 }
--- src/keychain.c
+++ src/keychain.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "elliptic.h"
 #include "ed25519.h"
 #include "secp256k1.h"
@@ -41,3 +43,69 @@ static void BIP32Hash_SHA3(const unsigned char chainCode[32], unsigned int nChil
     hmac_sha3_512_final(&hmac_ctx, output, 64);
 }
 
+int elliptic_hd_import_pub(EllipticHDContext *ctx, int type, const uint8_t binary[BIP32_EXTKEY_SIZE]) {
+    ctx->nDepth = binary[0];
+    memcpy(ctx->vchFingerprint, binary+1, 4);
+    ctx->nChild = (binary[5] << 24) | (binary[6] << 16) | (binary[7] << 8) | binary[8];
+    memcpy(ctx->chaincode, binary+9, 32);
+    return elliptic_init(&ctx->context, type, NULL, binary+41);
+}
+
+int elliptic_hd_import_priv(EllipticHDContext *ctx, int type, const uint8_t binary[BIP32_EXTKEY_SIZE]) {
+    ctx->nDepth = binary[0];
+    memcpy(ctx->vchFingerprint, binary+1, 4);
+    ctx->nChild = (binary[5] << 24) | (binary[6] << 16) | (binary[7] << 8) | binary[8];
+    memcpy(ctx->chaincode, binary+9, 32);
+    return elliptic_init(&ctx->context, type, binary+42, NULL);
+}
+
+int elliptic_hd_export_pub(EllipticHDContext *ctx, uint8_t binary[BIP32_EXTKEY_SIZE]) {
+    if (ctx->context.EllipticType != EllipticInvalid) {
+
+        // Create xpub binary representation
+
+        // Chain depth
+        binary[0] = ctx->nDepth;
+        // Save key fingerprint
+        memcpy(binary+1, ctx->vchFingerprint, 4);
+        // Child path
+        binary[5] = (ctx->nChild >> 24) & 0xFF; binary[6] = (ctx->nChild >> 16) & 0xFF;
+        binary[7] = (ctx->nChild >>  8) & 0xFF; binary[8] = (ctx->nChild >>  0) & 0xFF;
+        // Copy chain code
+        memcpy(binary+9, ctx->chaincode, 32);
+        // Public key prefix is always 0x03
+        binary[41] = 0x03;
+        // Copy public key
+        memcpy(binary+42, ctx->context.PublicKey, 33);
+
+        return 1;
+    }
+
+    return 0;
+}
+
+int elliptic_hd_export_priv(EllipticHDContext *ctx, uint8_t binary[BIP32_EXTKEY_SIZE]) {
+    if (ctx->context.HasPrivate) {
+
+        // Create xpriv binary representation
+        //  Note: public key is not exported
+
+        // Chain depth
+        binary[0] = ctx->nDepth;
+        // Save key fingerprint
+        memcpy(binary+1, ctx->vchFingerprint, 4);
+        // Child path
+        binary[5] = (ctx->nChild >> 24) & 0xFF; binary[6] = (ctx->nChild >> 16) & 0xFF;
+        binary[7] = (ctx->nChild >>  8) & 0xFF; binary[8] = (ctx->nChild >>  0) & 0xFF;
+        // Copy chain code
+        memcpy(binary+9, ctx->chaincode, 32); 
+        // Private key prefix is always 0x00
+        binary[41] = 0; 
+        // Copy private key
+        memcpy(binary+42, ctx->context.PrivateKey, 32);
+
+        return 1;
+    }
+
+    return 0;
+}
