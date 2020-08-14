b8e526fc286ad2629eba01344f1c09a06ae68a12
CryptoManiac <balthazar@yandex.ru>
2020-08-14 23:29:24 +0300

    Fix

--- include/elliptic.h
+++ include/elliptic.h
@@ -1,6 +1,10 @@
 #ifndef ELLIPTIC_H
 #define ELLIPTIC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 #include <stddef.h>
 
@@ -18,5 +22,9 @@ typedef struct EllipticContext {
 int elliptic_init(EllipticContext *ctx, int type, const uint8_t *key, const uint8_t *public_key);
 int elliptic_sign(EllipticContext *ctx, const uint8_t *digest, size_t digest_size, uint8_t *signature);
 int elliptic_verify(EllipticContext *ctx, const uint8_t *digest, size_t digest_size, uint8_t *signature, size_t signature_size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
